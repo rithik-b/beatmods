1a63ff2baf06a66c1ddd5e0156440b3f9752eb6d
PixelBoom <rithikbhatia12@gmail.com>
2024-01-14 14:14:43 -0500

    Fixed exception handling for logged out case

--- src/app/new-mod/page.tsx
+++ src/app/new-mod/page.tsx
@@ -2,12 +2,17 @@ import NewModForm from "@beatmods/app/new-mod/_components/NewModForm"
 import { api } from "@beatmods/trpc/server"
 
 export default async function NewMod() {
-  const user = await api.user.user.query()
+  let isLogged = false
+  try {
+    const user = await api.user.user.query()
+    isLogged = !!user
+  } catch (e) {}
+
   const categories = await api.categories.query()
 
   return (
     <main className="p-5">
-      {!!user ? (
+      {isLogged ? (
         <NewModForm categories={categories} />
       ) : (
         <p>You must be signed in to create a new mod.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import { api } from "@beatmods/trpc/server"
 import SignIn from "@beatmods/app/_components/SignIn"
 
 export default async function Home() {
-  const user = await api.user.user.query()
+  try {
+    await api.user.user.query()
 
-  if (!!user) {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="text-4xl">Welcome back!</h1>
       </main>
     )
+  } catch (e) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <SignIn />
+      </main>
+    )
   }
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <SignIn />
-    </main>
-  )
 }
