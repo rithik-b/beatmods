2262582d2f9588ae30a226fa374fe9b6bf31551a
PixelBoom <rithikbhatia12@gmail.com>
2024-01-27 00:14:26 -0500

    Made styling for spam's silly contribution better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,11 @@ export default async function Home() {
 
     return (
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex flex-row group">
+        <div className="group flex flex-row items-end gap-2">
           <h1 className="text-4xl">Welcome back!</h1>
-          <h3 className="text-2xl ml-2 text-gray-500 opacity-0 group-hover:opacity-100 transition-opacity duration-1000"> back from where?</h3>
+          <h3 className="text-2xl text-gray-500 opacity-0 transition-opacity duration-1000 group-hover:opacity-100">
+            back from where?
+          </h3>
         </div>
       </main>
     )
