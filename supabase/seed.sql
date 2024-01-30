23ff488af31fa1d3164d0f3c838dd96a60c21da6
Umbranox <itsumbra@protonmail.com>
2024-01-31 08:32:53 +1100

    Add basic seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  categories (name, description)
+VALUES
+  ('Core', 'Core Mods'),
+  (
+    'Leaderboards',
+    'Mods which change Beat Saber''s leaderboard experience'
+  );
+
+INSERT INTO 
+    game_versions (version, published) 
+VALUES 
+    ('1.29.1', 'true');
