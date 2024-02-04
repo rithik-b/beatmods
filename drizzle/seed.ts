624d2242c4305b620338eee9f17096af0880f8bb
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-02-04 18:08:00 +0000

    Added mock users
    Bocchi the Mock!

--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -1,4 +1,8 @@
-import { categoriesTable, gameVersionsTable } from "@beatmods/types/drizzle"
+import {
+  categoriesTable,
+  gameVersionsTable,
+  githubUsersTable,
+} from "@beatmods/types/drizzle"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 import { env } from "process"
@@ -26,6 +30,35 @@ await drizzleClient.insert(gameVersionsTable).values([
   {
     version: "1.34.2",
     published: true,
+  },
+])
+
+await drizzleClient.insert(githubUsersTable).values([
+  {
+    userName: "Bocchi",
+    name: "Hitori Gotoh",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/bocchi-the-rock/images/6/67/Hitori_Gotoh_Profile.png",
+  },
+  {
+    userName: "Nijika Ijichi",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/bocchi-the-rock/images/b/be/Nijika_Ijichi_Profile.png",
+  },
+  {
+    userName: "Ryo Yamada",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/bocchi-the-rock/images/1/1d/Ryo_Yamada_Profile.png",
+  },
+  {
+    userName: "Ikuyo Kita",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/bocchi-the-rock/images/7/7a/Ikuyo_Kita_Profile.png",
+  },
+  {
+    userName: "Kikuri Hiroi",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/bocchi-the-rock/images/8/88/Kikuri_Hiroi.png",
   },
 ])
 
