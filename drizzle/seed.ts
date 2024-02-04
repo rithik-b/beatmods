79b27ccc2ff52bf3dd55d480ddfb81a23d792b7d
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-02-04 22:03:27 +0000

    Added mock mod

--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -2,6 +2,10 @@ import {
   categoriesTable,
   gameVersionsTable,
   githubUsersTable,
+  modContributorsTable,
+  modVersionSupportedGameVersionsTable,
+  modVersionsTable,
+  modsTable,
 } from "@beatmods/types/drizzle"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
@@ -10,6 +14,10 @@ import { env } from "process"
 const connectionString = env.SUPABASE_DB_URL!
 const connection = postgres(connectionString)
 const drizzleClient = drizzle(connection)
+
+type MockId = {
+  id: string
+}
 
 await drizzleClient.insert(categoriesTable).values([
   {
@@ -22,43 +30,86 @@ await drizzleClient.insert(categoriesTable).values([
   },
 ])
 
-await drizzleClient.insert(gameVersionsTable).values([
+const mockGameVersions: MockId[] = await drizzleClient
+  .insert(gameVersionsTable)
+  .values([
+    {
+      version: "1.29.1",
+      published: true,
+    },
+    {
+      version: "1.34.2",
+      published: true,
+    },
+  ])
+  .returning()
+
+const mockUsers: MockId[] = await drizzleClient
+  .insert(githubUsersTable)
+  .values([
+    {
+      userName: "Bocchi",
+      name: "Hitori Gotoh",
+      avatarUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/6/67/Hitori_Gotoh_Profile.png",
+    },
+    {
+      userName: "Nijika Ijichi",
+      avatarUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/b/be/Nijika_Ijichi_Profile.png",
+    },
+    {
+      userName: "Ryo Yamada",
+      avatarUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/1/1d/Ryo_Yamada_Profile.png",
+    },
+    {
+      userName: "Ikuyo Kita",
+      avatarUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/7/7a/Ikuyo_Kita_Profile.png",
+    },
+    {
+      userName: "Kikuri Hiroi",
+      avatarUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/8/88/Kikuri_Hiroi.png",
+    },
+  ])
+  .returning()
+
+await drizzleClient.insert(modsTable).values([
   {
-    version: "1.29.1",
-    published: true,
-  },
-  {
-    version: "1.34.2",
-    published: true,
+    id: "Bocchi the Mod",
+    name: "Bocchi the Mod",
+    slug: "bocchi-the-mod",
+    description: "Adds social anxiety to the game (just like Bocchi fr fr)",
+    moreInfoUrl: "https://fortnite.com",
+    category: "Core",
   },
 ])
 
-await drizzleClient.insert(githubUsersTable).values([
+const mockModVersion: MockId[] = await drizzleClient
+  .insert(modVersionsTable)
+  .values([
+    {
+      modId: "Bocchi the Mod",
+      version: "1.0.0",
+      downloadUrl:
+        "https://static.wikia.nocookie.net/bocchi-the-rock/images/3/32/Episode_2-3.png",
+    },
+  ])
+  .returning()
+
+await drizzleClient.insert(modVersionSupportedGameVersionsTable).values([
   {
-    userName: "Bocchi",
-    name: "Hitori Gotoh",
-    avatarUrl:
-      "https://static.wikia.nocookie.net/bocchi-the-rock/images/6/67/Hitori_Gotoh_Profile.png",
+    modVersionId: mockModVersion[0]?.id ?? "",
+    gameVersionId: mockGameVersions[1]?.id ?? "",
   },
+])
+
+await drizzleClient.insert(modContributorsTable).values([
   {
-    userName: "Nijika Ijichi",
-    avatarUrl:
-      "https://static.wikia.nocookie.net/bocchi-the-rock/images/b/be/Nijika_Ijichi_Profile.png",
-  },
-  {
-    userName: "Ryo Yamada",
-    avatarUrl:
-      "https://static.wikia.nocookie.net/bocchi-the-rock/images/1/1d/Ryo_Yamada_Profile.png",
-  },
-  {
-    userName: "Ikuyo Kita",
-    avatarUrl:
-      "https://static.wikia.nocookie.net/bocchi-the-rock/images/7/7a/Ikuyo_Kita_Profile.png",
-  },
-  {
-    userName: "Kikuri Hiroi",
-    avatarUrl:
-      "https://static.wikia.nocookie.net/bocchi-the-rock/images/8/88/Kikuri_Hiroi.png",
+    modId: "Bocchi the Mod",
+    userId: mockUsers[0]?.id ?? "",
   },
 ])
 
